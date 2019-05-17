5c4d6beb7157e7ccfb874c458c8a1839d196ce94
Derek Wang <guanghuiw@vmware.com>
2019-05-17 11:19:29 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
