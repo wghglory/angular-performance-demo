e5d7c6e3cdcf964ae1c6f045c527d8040c54dade
Derek Wang <guanghuiw@vmware.com>
2019-06-03 16:38:46 +0800

    angular ChangeDetectionStrategy.OnPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-zone';
+export class AppComponent implements OnInit {
+  currentId = null;
+  boxes = [];
+  offsetX;
+  offsetY;
+
+  ngOnInit() {
+    for (let i = 0; i < 10000; i++) {
+      const id = i;
+      const x = getRandomInt(0, 500);
+      const y = getRandomInt(0, 500);
+      const box = {
+        id,
+        x,
+        y,
+      };
+      this.boxes.push(box);
+    }
+  }
+
+  mouseDown(event) {
+    const id = Number(event.target.getAttribute('dataId'));
+    const box = this.boxes[id];
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    this.offsetX = box.x - mouseX;
+    this.offsetY = box.y - mouseY;
+    this.currentId = id;
+  }
+
+  mouseMove(event) {
+    event.preventDefault();
+    if (this.currentId !== null) {
+      this.updateBox(this.currentId, event.clientX + this.offsetX, event.clientY + this.offsetY);
+    }
+  }
+
+  mouseUp($event) {
+    this.currentId = null;
+  }
+
+  updateBox(id, x, y) {
+    // const box = this.boxes[id];
+    // box.x = x;
+    // box.y = y;
+
+    this.boxes[id] = { id, x, y }; // new references instead of mutation due to OnPush
+  }
 }
--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-box',
+  // tslint:disable-next-line:component-selector
+  selector: '[app-box]',
   templateUrl: './box.component.html',
-  styleUrls: ['./box.component.scss']
+  styleUrls: ['./box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush, // set to OnPush
+  // The idea is to make Angular only check a component’s view bindings if one of its inputs have changed.
+  // requirement for OnPush: Make sure that BoxComponent’s input values are immutable.
 })
 export class BoxComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() box;
+  @Input() selected;
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
